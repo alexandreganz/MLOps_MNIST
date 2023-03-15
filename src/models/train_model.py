a287629d0fe24a988bea2935ce36c16481f4e57f
Alexandre <alexandre_muchinski@hotmail.com>
2023-03-15 14:29:58 +0100

    updated files with experiments from mlflow

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -6,6 +6,21 @@ import torch
 from src.data.make_dataset import CorruptMnist
 from src.models.model import MyAwesomeModel
 
+import mlflow
+import mlflow.pytorch
+from mlflow import MlflowClient
+
+import math
+
+def print_auto_logged_info(r):
+    tags = {k: v for k, v in r.data.tags.items() if not k.startswith("mlflow.")}
+    artifacts = [f.path for f in MlflowClient().list_artifacts(r.info.run_id, "model")]
+    print("run_id: {}".format(r.info.run_id))
+    print("artifacts: {}".format(artifacts))
+    print("params: {}".format(r.data.params))
+    print("metrics: {}".format(r.data.metrics))
+    print("tags: {}".format(tags))
+
 
 def training() -> None:
     parser = argparse.ArgumentParser(description="Training arguments")
@@ -34,15 +49,21 @@ def training() -> None:
             loss.backward()
             optimizer.step()
             loss_tracker.append(loss.item())
+            
         print(f"Epoch {epoch+1}/{n_epoch}. Loss: {loss}")
+        
+
+        
+        
     torch.save(model.state_dict(), "models/trained_model.pt")
 
     plt.plot(loss_tracker, "-")
     plt.xlabel("Training step")
     plt.ylabel("Training loss")
     plt.savefig(f"reports/figures/training_curve.png")
-
-
+    
+    
 # testing the gcloud trigger
 if __name__ == "__main__":
     training()
+    
